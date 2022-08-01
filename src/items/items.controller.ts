0c74b72e4aaea5af4a6742e76ff033f781d29117
Tahara Kazuki <ssp0727.lnc@gmail>
2022-08-01 17:53:00 +0900

    update

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { ItemStatus } from './type/item-status.enum';
 import { Item } from './type/item.model';
@@ -32,5 +40,15 @@ export class ItemsController {
     };
 
     return this.itemsService.create(item);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string) {
+    return this.itemsService.updateStatus(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): void {
+    this.itemsService.delete(id);
   }
 }
