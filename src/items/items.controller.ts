3bc17c930f7e2af9b9253a75b7f9d8f7c027062b
Tahara Kazuki <ssp0727.lnc@gmail>
2022-08-01 16:21:40 +0900

    update

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,11 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { ItemStatus } from './type/item-status.enum';
+import { Item } from './type/item.model';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
   @Get()
-  findAll() {
+  findAll(): Item[] {
     return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+
+    return this.itemsService.create(item);
   }
 }
